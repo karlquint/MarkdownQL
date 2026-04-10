5c17cfdf3006276df0c7c2bf36ce6d4abb271b13
Karl Quint <karl.quint@icloud.com>
2026-04-10 18:05:59 +0200

    Initial Commit

---
+++ MarkdownQL/MarkdownQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownQLApp.swift
+//  MarkdownQL
+//
+//  Created by Karl Quint on 10.04.26.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
